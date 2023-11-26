cfec2aa44f152b93d90a127425bacf5b91cba6ec
juhaamid <juhaamid@student.42abudhabi.ae>
2023-11-26 12:38:10 +0400

    dad

--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: juhaamid <juhaamid@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 13:14:18 by juhaamid          #+#    #+#             */
-/*   Updated: 2023/11/24 20:31:07 by juhaamid         ###   ########.fr       */
+/*   Updated: 2023/11/26 12:35:57 by juhaamid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,45 +19,53 @@ Harl::~Harl(){
 	
 }
 void	Harl::debug(void){
-	std::cout << "DEBUG: I love having extra bacon for my 7XL-double-cheese ";
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese ";
 	std::cout << "-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void	Harl::info(void){
-	std::cout << "INFO :I cannot believe adding extra bacon costs more money. You didn't put";
-	std::cout << "enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put";
+	std::cout << " enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
 }
 
 void	Harl::warning(void){
-	std::cout << "WARNING: I think I deserve to have some extra bacon for free. I've been coming for";
-	std::cout << "years whereas you started working here since last month." << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I've been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void	Harl::error(void){
-	std::cout << "ERROR: This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 // add an array of functions
 
 void	Harl::complain(std::string level){
 	
-	int i = 0;
+	int i = 4;
 	
 	std::string _list[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	void(Harl::*fnction[4])()= {
-		&Harl::debug,
-		&Harl::info,
-		&Harl::warning,
-		&Harl::error
-	};
-	while (i < 4)
-	{
+	for(i = 0; i < 4; i++)
 		if (level == _list[i])
-		{
-			(this->*fnction[i])();
-			return ;
-		}
-		i++;
-	}	
-	std::cout << "I don't know what to say about " << level << std::endl;;
+			break;
+	switch (i)
+	{
+		case 0:
+			this->debug();
+			break;
+		case 1:
+			this->info();
+			break;
+		case 2:
+			this->warning();
+			break;
+		case 3:
+			this->error();
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
 }
